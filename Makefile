5dca3e83b70f918322d7b8366cb48274e02a642d
Wendelin hans Weisser <wweisser@3-F-11.42heilbronn.de>
2022-04-20 19:25:29 +0200

    20.4.2022

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: wweisser <wweisser@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/23 14:02:57 by wweisser          #+#    #+#              #
-#    Updated: 2022/04/06 09:11:34 by wweisser         ###   ########.fr        #
+#    Updated: 2022/04/20 17:58:35 by wweisser         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -29,84 +29,6 @@ $(NAME): $(SRC) libft.h
 
 ft_*.o: ft_*.c
 	cc -Wall -Wextra -Werror -c ft_*.c
-
-# ft_isalpha.o: ft_isalpha.c
-# 	cc -Wall -Wextra -Werror -c ft_isalpha.c
-
-# ft_isdigit.o: ft_isdigit.c
-# 	cc -Wall -Wextra -Werror -c ft_isdigit.c
-
-# ft_isalnum.o: ft_isalnum.c
-# 	cc -Wall -Wextra -Werror -c ft_isalnum.c
-
-# ft_isascii.o: ft_isascii.c
-# 	cc -Wall -Wextra -Werror -c ft_isascii.c
-
-# ft_isprint.o: ft_isprint.c
-# 	cc -Wall -Wextra -Werror -c ft_isprint.c
-
-# ft_memset.o: ft_memset.c
-# 	cc -Wall -Wextra -Werror -c ft_memset.c
-
-# ft_bzero.o: ft_bzero.c
-# 	cc -Wall -Wextra -Werror -c ft_bzero.c
-
-# ft_memcpy.o: ft_memcpy.c
-# 	cc -Wall -Wextra -Werror -c ft_memcpy.c
-
-# ft_memmove.o: ft_memmove.c
-# 	cc -Wall -Wextra -Werror -c ft_memmove.c
-
-# ft_memchr.o: ft_memchr.c
-# 	cc -Wall -Wextra -Werror -c ft_memchr.c
-
-# ft_memcmp.o: ft_memcmp.c
-# 	cc -Wall -Wextra -Werror -c ft_memcmp.c
-
-# ft_toupper.o: ft_toupper.c
-# 	cc -Wall -Wextra -Werror -c ft_toupper.c
-
-# ft_tolower.o: ft_tolower.c
-# 	cc -Wall -Wextra -Werror -c ft_tolower.c
-
-# ft_strchr.o: ft_strchr.c
-# 	cc -Wall -Wextra -Werror -c ft_strchr.c
-
-# ft_strlen.o: ft_strlen.c
-# 	cc -Wall -Wextra -Werror -c ft_strlen.c
-
-# ft_strlcat.o: ft_strlcat.c
-# 	cc -Wall -Wextra -Werror -c ft_strlcat.c
-
-# ft_strncmp.o: ft_strncmp.c
-# 	cc -Wall -Wextra -Werror -c ft_strncmp.c
-
-# ft_strnstr.o: ft_strnstr.c
-# 	cc -Wall -Wextra -Werror -c ft_strnstr.c
-
-# ft_strlcpy.o: ft_strlcpy.c
-# 	cc -Wall -Wextra -Werror -c ft_strlcpy.c
-
-# ft_strrchr.o: ft_strrchr.c
-# 	cc -Wall -Wextra -Werror -c ft_strrchr.c
-
-# ft_strdup.o: ft_strdup.c
-# 	cc -Wall -Wextra -Werror -c ft_strdup.c
-
-# ft_substr.o: ft_substr.c
-# 	cc -Wall -Wextra -Werror -c ft_substr.c
-
-# ft_putchar_fd.o: ft_putchar_fd.c
-# 	cc -Wall -Wextra -Werror -c ft_putchar_fd.c
-
-# ft_putstr_fd.o: ft_putstr_fd.c
-# 	cc -Wall -Wextra -Werror -c ft_putstr_fd.c
-
-# ft_itoa.o: ft_itoa.c
-# 	cc -Wall -Wextra -Werror -c ft_itoa.c
-
-# ft_putendl_fd.o: ft_putendl_fd.c
-# 	cc -Wall -Wextra -Werror -c ft_putendl_fd.c
 
 main.o: main.c
 	cc -Wall -Wextra -Werror -c main.c
--- ft_split.c
+++ ft_split.c
@@ -6,34 +6,33 @@
 /*   By: wweisser <wweisser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/29 20:54:51 by wweisser          #+#    #+#             */
-/*   Updated: 2022/04/14 15:58:12 by wweisser         ###   ########.fr       */
+/*   Updated: 2022/04/20 19:09:03 by wweisser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
+#include <stdio.h>
 
-static void	clean(char **field, int numbstr)
+static void	asgnstr(char const *s, char **field, int i[3])
 {
-	while (numbstr-- >= 0)
-		free (field[numbstr]);
-	free (field);
-}
+	int ref;
 
-static int	nxtl(char const *s, int i, char c)
-{
-	int	lenstr;
-
-	lenstr = 0;
-	while (s[i] != '\0')
+	field[i[1]] = NULL;
+	field[i[1]] = (char *)malloc(sizeof(char) * (i[2] + 1));
+	if (field[i[1]] == NULL)
 	{
-		if (s[i] != c)
-			lenstr++;
-		if (s[i] != c && (s[i + 1] == '\0' || s[i + 1] == c))
-			return (lenstr++);
-		i++;
+		while (i[1] >= 0)
+		{
+			free ((void *)field[i[1]]);
+			field[1] = NULL;
+			i[1]--;
+		}
+		free ((void **)field);
+		field = NULL;
 	}
-	return (0);
+	else
+		ref = ft_strlcpy(field[i[1]], s + (i[0] - i[2] + 1), i[2] + 1);
 }
 
 static char	**numbstr(char **field, char const *s, char c)
@@ -51,12 +50,12 @@ static char	**numbstr(char **field, char const *s, char c)
 	field = (char **)malloc(sizeof(char *) * (i[1] + 1));
 	if (field == NULL)
 	{
-		free (field);
-		return (field);
+		free ((void **)field);
+		return (NULL);
 	}
 	while (i[1] >= 0)
 	{
-		field[i[1]] = 0;
+		field[i[1]] = NULL;
 		i[1]--;
 	}
 	return (field);
@@ -65,38 +64,34 @@ static char	**numbstr(char **field, char const *s, char c)
 char	**ft_split(char const *s, char c)
 {
 	char	**field;
-	int		i[2];
-	int		deflt;
+	int		i[3];
 
 	field = NULL;
 	if (s != NULL)
 		field = numbstr(field, s, c);
 	i[0] = 0;
 	i[1] = 0;
+	i[2] = 0;
 	while (field != NULL && s[i[0]] != '\0')
 	{
 		if (s[i[0]] != c)
+			i[2]++;
+		if (s[i[0]] != c && (s[i[0] + 1] == c || s[i[0] + 1] == '\0'))
 		{
-			field[i[1]] = (char *)malloc(sizeof(char) * nxtl(s, i[0], c) + 1);
-			if (field[i[1]] != NULL)
-				deflt = ft_strlcpy(field[i[1]], s + i[0], nxtl(s, i[0], c) + 1);
-			else
-				clean (field, i[1]);
-			i[0] = i[0] + nxtl(s, i[0], c);
+			asgnstr(s, field, i);
 			i[1]++;
+			i[2] = 0;
 		}
-		else
 		i[0]++;
 	}
 	return (field);
 }
 
 // #include <stdio.h>
-// #include <stdio.h>
 // int	main(void)
 // {
 // 	char **t;
-// 	t = ft_split("  tt  tttt", ' ');
+// 	t = ft_split("t_", '_');
 // /*	printf("%d\n", nextdel("222   ", 0, ' '));*/
-// 	printf("str 1: %s\nstr 2: %s \n ", t[0], t[2]);
+// 	printf("str 1: %s\nstr 2: %s \n ", t[0], t[1]);
 // }
